e7eccdffde8dcc259fe62e1456bb2d3d47edd1aa
bob-takuya <tk3bob@gmail.com>
2025-06-18 16:32:57 +0900

    fix: Remove unused PutObjectCommand import
    - Remove unused PutObjectCommand from @aws-sdk/client-s3 import
    - Fix ESLint error: 'PutObjectCommand' is defined but never used

--- src/services/s3.ts
+++ src/services/s3.ts
@@ -1,4 +1,4 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client } from '@aws-sdk/client-s3'
 import type { ProjectConfig } from '@/types'
 
 export interface SyncResult {
